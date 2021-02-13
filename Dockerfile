f8f86d79780dde0742953defc15b90b806c5c8b1
takerukyono <respect-flea@i.softbank.jp>
2021-02-14 03:14:40 +0900

    .pyを作成

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM tensorflow/tensorflow
+
+# Japanese localization
+RUN apt -y update && \
+    apt -y upgrade && \
+    apt -y install language-pack-ja-base language-pack-ja ibus-mozc && \
+    locale-gen ja_JP.UTF-8 && \
+    echo "export LANG=ja_JP.UTF-8" >> ~/.bashrc
+
+# Install utils without interactive dialogue
+RUN DEBIAN_FRONTEND=noninteractive apt -y install vim
+
+# Install Python Library
+RUN python3 -m pip install --upgrade \
+    pip \
+    matplotlib
+
+# Add User
+RUN useradd -m test
+USER test
--- nn-hidden_sigmoid.py
+++ nn-hidden_sigmoid.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
+import matplotlib.pyplot as plt 
 
 # 学習回数
 epochs=100
@@ -11,19 +12,17 @@ num_hidden = 3
 
 # データ生成
 x = np.arange(-10, 10, 0.1) 
-# input_data = (x - x.min()) / (x.max() - x.min())
-input_data = x  / x.max()
-# input_data = (x - x.mean()) / x.std()
-training_data = 4 * input_data ** 3 - 2 * input_data
-# training_data = (y - y.min()) / (y.max() - y.min())
 
-"""
+input_data = x  / x.max() # 
+# input_data = (x - x.min()) / (x.max() - x.min()) # 正規化
+# input_data = (x - x.mean()) / x.std() # 標準化
+training_data = 4 * input_data ** 3 - 2 * input_data # 目標関数
+
 # 教師データグラフ出力
 plt.title('train_data')
 plt.plot(input_data, training_data, linestyle="dashed")
 plt.savefig('images/train_data.png')
 plt.show()
-"""
 
 # モデル生成
 model = keras.Sequential(
@@ -42,17 +41,37 @@ model.compile(
     metrics='mse' # 評価関数、平均二乗誤差
 )
 
+
 # 学習
 print("Fit model on training data")
 history = model.fit(
     input_data,
     training_data,
     batch_size=1,
-    epochs=epochs,
-    # validation_data = (input_data,training_data)
+    epochs=epochs
 )
 
 # 検証
 print("Evaluate on test data")
 results = model.predict(input_data)
 # print("test loss, test acc:", results)
+
+# 誤差関数グラフ出力
+plt.plot(history.history['loss'])
+plt.title('model loss')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.savefig('images/model_loss-epochs_' + str(epochs) +'-num_hidden_' + str(num_hidden) + '.png')
+plt.show()
+
+
+# 結果グラフ出力
+plt.title('result epochs=' + str(epochs))
+plt.plot(input_data, training_data, linestyle="dashed", label="training_data")
+plt.plot(input_data, results, marker="+", label="results")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.savefig('images/result-epochs_' + str(epochs) +'-num_hidden_' + str(num_hidden) + '.png')
+plt.legend(loc = 'best')
+plt.show
+
